dd4b0c98a0a3b280eecf3b2732d661fe032882ee
ZapDX <149316285+ZapDX@users.noreply.github.com>
2025-11-25 19:21:13 +0700

    memperbaiki bug di server.js ("path" harusnya tidak ada spasi didalamnya)

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const path = require(' path');
+const path = require('path');
 const cors = require('cors');
 
 const authRoutes = require('./routes/auth');
